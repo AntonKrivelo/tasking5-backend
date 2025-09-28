6755c20ddcec089b0776afc7a55329bccb159d78
Anton Krivelo <krivelo2017@mail.ru>
2025-09-28 18:48:38 +0300

    asd

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const PORT = 4000;
+
+//cors
+
+app.use(cors());
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({ msg: 'This is CORS-enabled for all origins!' });
+});
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +37,34 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
+// //post
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request');
+});
+
+// app.post('/register', (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   if (!name || !email || !password) {
+//     return res.status(400).json({ message: 'Заполните все поля' });
+//   }
+
+//   // Здесь можно добавить сохранение в базу
+//   console.log('Получены данные:', { name, email, password });
+
+//   res.status(201).json({ message: 'Регистрация успешна' });
+// });
+
+// app.listen(4000, () => {
+//   console.log(`Сервер работает на http://localhost:4000`);
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
